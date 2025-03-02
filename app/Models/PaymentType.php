58b9949e2b52dc8963ad9863cecaae7dea985737
Vinicius Henrique dos Santos <viniciushenrique.dev@gmail.com>
2025-03-02 19:12:33 -0300

    feat(models): create model to payment_types table

---
+++ app/Models/PaymentType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentType extends Model
+{
+    /**
+     * The table associated with model.
+     *
+     * @var string
+     */
+    protected $table = 'payment_types';
+}
