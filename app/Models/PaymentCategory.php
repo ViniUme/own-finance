c106407480513bc01d8a6bfdb6eadf8a741fbc88
Vinicius Henrique dos Santos <viniciushenrique.dev@gmail.com>
2025-03-02 18:44:41 -0300

    feat(models): create model to payment_categories table

---
+++ app/Models/PaymentCategory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentCategory extends Model
+{
+    /**
+     * The table associated with model.
+     *
+     * @var string
+     */
+    protected $table = 'payment_categories';
+}
