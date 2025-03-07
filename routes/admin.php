618d2ce519d0c8afea58e1dcc939d9d6ab925cf0
Vinicius Henrique dos Santos <viniciushenrique.dev@gmail.com>
2025-03-06 22:09:17 -0300

    feat(routes & middlewares)!: create dashboard route and apply admin middleware

--- routes/admin.php
+++ routes/admin.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function () {
-    Route::get('login', function () {
+Route::prefix('admin')->middleware(['admin'])->group(function () {
+    Route::get('/login', function () {
         return response('', 200);
     })->name('admin.login');
+
+    Route::get('/dashboard', function(){
+        return response('', 200);
+    })->name('admin.dashboard');
 });
