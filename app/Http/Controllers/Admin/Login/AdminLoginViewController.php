6bd3e093c67b667dfdbadc917888d0328fc9dca8
Vinicius Henrique dos Santos <viniciushenrique.dev@gmail.com>
2025-03-23 01:27:45 -0300

    feat: create controller to admin login route

---
+++ app/Http/Controllers/Admin/Login/AdminLoginViewController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Login;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminLoginViewController extends Controller
+{
+    public function __invoke()
+    {
+        return;
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\Login\AdminLoginViewController;
 use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function () {
-    Route::get('/login', function () {
-        return response('', 200);
-    })->name('admin.login');
+    Route::get('/login', AdminLoginViewController::class)->name('admin.login');
 
     Route::middleware([Admin::class])->group(function () {
         Route::get('/dashboard', function(){
