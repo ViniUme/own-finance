ba77399a0cece1b9fbce2eac7e326e3fd8eff46e
Vinicius Henrique dos Santos <viniciushenrique.dev@gmail.com>
2025-03-30 02:20:05 -0300

    test(auth): create test to assert status 422 in login without password

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -33,6 +33,16 @@ class AuthTest extends TestCase
         $response->assertStatus(422);
     }
 
+    public function test_login_without_password(): void
+    {
+        $response = $this->postJson(route('admin.login.auth'), [
+            'email' => 'test@test.com',
+            'password' => ''
+        ]);
+
+        $response->assertStatus(422);
+    }
+
     public function test_login_with_undefined_email(): void
     {
         $this->seed(DatabaseSeeder::class);
