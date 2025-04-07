f5c51bf1fb9c3b09ff1abed717998886e332cb5e
Vinicius Henrique dos Santos <viniciushenrique.dev@gmail.com>
2025-04-06 21:27:55 -0300

    chore(controller): add response config in AdminLoginAuthController

--- app/Http/Controllers/Admin/Login/AdminLoginAuthController.php
+++ app/Http/Controllers/Admin/Login/AdminLoginAuthController.php
@@ -3,16 +3,13 @@
 namespace App\Http\Controllers\Admin\Login;
 
 use App\Http\Controllers\BaseController;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Login\AdminLoginAuthRequest;
-use App\Models\User;
-use Exception;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 class AdminLoginAuthController extends BaseController
 {
-    public function __invoke(AdminLoginAuthRequest $request)
+    public function __invoke(AdminLoginAuthRequest $request): JsonResponse
     {
         if (Auth::attempt($request->only('email', 'password'), $request->remember_me)) {
             $request->session()->regenerate();
