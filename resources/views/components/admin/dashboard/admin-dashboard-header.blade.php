4f1a3e9700f2388efb8685d6b547d07655132f3d
Vinicius Henrique dos Santos <viniciushenrique.dev@gmail.com>
2025-04-13 11:46:53 -0300

    chore: add user name and configure logout button in header

--- resources/views/components/admin/dashboard/admin-dashboard-header.blade.php
+++ resources/views/components/admin/dashboard/admin-dashboard-header.blade.php
@@ -219,8 +219,7 @@
                                 <div class="d-flex flex-row align-items-center gap-2">
                                     <img src="{{ asset('assets/images/avatars/06.png') }}" alt="" class="rounded-circle" width="54" height="54">
                                     <div class="">
-                                        <h6 class="mb-0 dropdown-user-name">Jhon Deo</h6>
-                                        <small class="mb-0 dropdown-user-designation text-secondary">UI Developer</small>
+                                        <h6 class="mb-0 dropdown-user-name">{{ $user->name }}</h6>
                                     </div>
                                 </div>
                             </a>
@@ -229,65 +228,12 @@
                             <hr class="dropdown-divider">
                         </li>
                         <li>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="d-flex align-items-center">
-                                <div class="">
-                                    <ion-icon name="person-outline"></ion-icon>
-                                </div>
-                                <div class="ms-3"><span>Profile</span></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="d-flex align-items-center">
-                                <div class="">
-                                    <ion-icon name="settings-outline"></ion-icon>
-                                </div>
-                                <div class="ms-3"><span>Setting</span></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="d-flex align-items-center">
-                                <div class="">
-                                    <ion-icon name="speedometer-outline"></ion-icon>
-                                </div>
-                                <div class="ms-3"><span>Dashboard</span></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="d-flex align-items-center">
-                                <div class="">
-                                    <ion-icon name="wallet-outline"></ion-icon>
-                                </div>
-                                <div class="ms-3"><span>Earnings</span></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="d-flex align-items-center">
-                                <div class="">
-                                    <ion-icon name="cloud-download-outline"></ion-icon>
-                                </div>
-                                <div class="ms-3"><span>Downloads</span></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="javascript:;">
+                            <a class="dropdown-item" href="{{ route('admin.logout') }}">
                                 <div class="d-flex align-items-center">
                                 <div class="">
                                     <ion-icon name="log-out-outline"></ion-icon>
                                 </div>
-                                <div class="ms-3"><span>Logout</span></div>
+                                <div class="ms-3"><span>Sair</span></div>
                                 </div>
                             </a>
                         </li>
