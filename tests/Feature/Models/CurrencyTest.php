d7def6e5a7a792fcf587114675607c6bb4ddf9d7
ViniUme <viniciushenrique.dev@gmail.com>
2025-08-18 21:09:07 -0300

    test(models): check if can create a currency

--- tests/Feature/Models/CurrencyTest.php
+++ tests/Feature/Models/CurrencyTest.php
@@ -3,14 +3,29 @@
 namespace Tests\Feature\Models;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Currency;
 
 class CurrencyTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_currency_model_exists(): void
     {
         $this->assertTrue(class_exists(Currency::class));
+    }
+
+    public function test_create_currency(): void
+    {
+        $currency = Currency::create([
+            'code' => 'BRL',
+            'name' => 'Real Brasileiro',
+            'decimal_places' => '2',
+        ]);
+
+        $this->assertInstanceOf(Currency::class, $currency);
+        $this->assertEquals('BRL', $currency->code);
+        $this->assertEquals('Real Brasileiro', $currency->name);
+        $this->assertEquals('2', $currency->decimal_places);
     }
 }
