9897188986fc7013d143756a732ddb780d971d5a
ViniUme <viniciushenrique.dev@gmail.com>
2025-08-19 22:41:10 -0300

    feat(database): create migration to generate accounts table

---
+++ database/migrations/2025_08_20_004938_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('currency_id')->constrained();
+            $table->string('name');
+            $table->enum('type', ['wallet', 'bank', 'investment']);
+            $table->string('color_hex', length: 6)->nullable();
+            $table->bigInteger('current_balance')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
