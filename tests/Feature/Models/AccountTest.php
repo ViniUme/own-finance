762e9a5a7a771b7d65aeb93fe1048676f7acf9b9
ViniUme <viniciushenrique.dev@gmail.com>
2025-08-19 22:44:57 -0300

    test(model): check if accounts model exists

--- tests/Feature/Models/AccountTest.php
+++ tests/Feature/Models/AccountTest.php
@@ -6,11 +6,17 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Account;
 
 class AccountTest extends TestCase
 {
     public function test_accounts_table_exists(): void
     {
         $this->assertTrue(Schema::hasTable('accounts'));
+    }
+
+    public function test_account_model_exists(): void
+    {
+        $this->assertTrue(class_exists(Account::class));
     }
 }
