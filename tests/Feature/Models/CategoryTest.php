9ab11f835d534cccabe37b98e8273286887c218e
ViniUme <viniciushenrique.dev@gmail.com>
2025-08-24 02:21:00 -0300

    feat(database): create categories table

--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature\Models;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_category_table_exists(): void
     {
         $this->assertTrue(Schema::hasTable('categories'));
