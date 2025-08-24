5e7a34f78f9ebaaf43aba489ee71efd395174e44
ViniUme <viniciushenrique.dev@gmail.com>
2025-08-23 21:46:20 -0300

    test(model): check if can create account

--- tests/Feature/Models/AccountTest.php
+++ tests/Feature/Models/AccountTest.php
@@ -7,9 +7,13 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Schema;
 use App\Models\Account;
+use App\Models\Currency;
+use App\Models\User;
 
 class AccountTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_accounts_table_exists(): void
     {
         $this->assertTrue(Schema::hasTable('accounts'));
@@ -18,5 +22,26 @@ class AccountTest extends TestCase
     public function test_account_model_exists(): void
     {
         $this->assertTrue(class_exists(Account::class));
+    }
+
+    public function test_create_account(): void
+    {
+        $user = User::factory()->create();
+        $currency = Currency::create([
+            'code' => 'BRL',
+            'name' => 'Real Brasileiro',
+            'decimal_places' => 2
+        ]);
+
+        $account = Account::create([
+            'user_id' => $user->id,
+            'currency_id' => $currency->id,
+            'name' => 'Account Test',
+            'type' => 'bank'
+        ]);
+        $this->assertEquals($user->id, $account->user_id);
+        $this->assertEquals($currency->id, $account->currency_id);
+        $this->assertEquals('Account Test', $account);
+        $this->assertEquals('bank', $account->type);
     }
 }
