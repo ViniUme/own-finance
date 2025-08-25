a4266afd7a0b2ee9c6e5f0318c955d9d7e9594f6
ViniUme <viniciushenrique.dev@gmail.com>
2025-08-25 03:25:58 -0300

    feat(database): create migration to generate movements table

---
+++ database/migrations/2025_08_25_042955_create_movements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('movements', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignId('account_id')->constrained();
+            $table->foreignId('currency_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->uuid('parent_id')->nullable();
+            $table->bigInteger('amount')->nullable();
+            $table->enum('type', ['income', 'expense', 'transfer']);
+            $table->string('description')->nullable();
+            $table->timestamp('date')->nullable();
+            $table->bigInteger('previous_balance');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('movements', function (Blueprint $table) {
+            $table->foreign('parent_id')->references('id')->on('movements');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('movements');
+    }
+};
--- tests/Feature/Models/MovementsTest.php
+++ tests/Feature/Models/MovementsTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature\Models;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
 class MovementsTest extends TestCase
 {
+    use RefreshDatabase;
+    
     public function test_movements_table_exists(): void
     {
         $this->assertTrue(Schema::hasTable('movements'));
