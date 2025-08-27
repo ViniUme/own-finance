c0f9b80bf244d1e4741f412fdeca8b7a5f87af1f
ViniUme <viniciushenrique.dev@gmail.com>
2025-08-27 20:28:31 -0300

    test(model): check if can create role

--- tests/Feature/Models/RoleTest.php
+++ tests/Feature/Models/RoleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Role;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
@@ -18,5 +19,16 @@ class RoleTest extends TestCase
     public function test_role_model_exists(): void
     {
         $this->assertTrue(class_exists('App\Models\Role'));
+    }
+
+    public function test_create_role(): void
+    {
+        $role = Role::create([
+            'name' => 'Test Role'
+        ]);
+
+        $this->assertNotEmpty($role->id);
+        $this->assertEquals('Test Role', $role->name);
+        $this->assertNotEmpty($role->created_at);
     }
 }
