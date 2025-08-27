c0072a10feb5d821919238718197b5d01db73c0b
ViniUme <viniciushenrique.dev@gmail.com>
2025-08-27 20:11:04 -0300

    test(model): check if model role exists

--- tests/Feature/Models/RoleTest.php
+++ tests/Feature/Models/RoleTest.php
@@ -9,9 +9,14 @@ use Tests\TestCase;
 class RoleTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     public function test_roles_table_exists(): void
     {
         $this->assertTrue(Schema::hasTable('roles'));
+    }
+
+    public function test_role_model_exists(): void
+    {
+        $this->assertTrue(class_exists('App/Models/Role'));
     }
 }
