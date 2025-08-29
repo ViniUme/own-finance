394b1587d641de814617a94ded6d27c12a8cecd7
ViniUme <viniciushenrique.dev@gmail.com>
2025-08-29 16:47:22 -0300

    feat(seeder): configure seeder to create admin user with role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,25 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->count(2)->sequence(
-            [
-                'name' => 'Admin',
-                'email' => 'admin@test.com',
-                'password' => Hash::make('admin123')
-            ],
-            [
-                'name' => 'Test',
-                'email' => 'test@test.com',
-                'password' => Hash::make('test123')
-            ]
-        )->create();
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password' => Str::uuid7()
+        ]);
+
+        $role = Role::factory()->create();
+
+        $user->roles()->attach($role->id);
     }
 }
