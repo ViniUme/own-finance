c6ce2df2621369bd2a478b92f959a7e4abf0f662
ViniUme <viniciushenrique.dev@gmail.com>
2025-08-30 14:33:43 -0300

    test(routes): check with home needs auth and redirect to login page

---
+++ tests/Feature/Routes/WebRoutesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\Routes;
+
+use Tests\TestCase;
+
+class WebRoutesTest extends TestCase
+{
+    public function test_home_route_needs_auth(): void
+    {
+        $response = $this->get(route('home'));
+        $response->assertStatus(307);
+        $response->assertRedirect(route('login'));
+    }
+}
