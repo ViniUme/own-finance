ef799ebd87561c09f77605ed1b344daf6771b162
ViniUme <viniciushenrique.dev@gmail.com>
2025-09-01 20:51:25 -0300

    test(routes): check if login authentication route exists

---
+++ tests/Feature/Routes/ApiRoutesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\Routes;
+
+use Illuminate\Support\Facades\Route;
+use Tests\TestCase;
+
+class ApiRoutesTest extends TestCase
+{
+    public function test_login_authentication_route_exists(): void
+    {
+        $routeExists = Route::has('login.authentication');
+        $this->assertTrue($routeExists);
+    }
+}
