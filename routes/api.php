16475447044a1e49053b858b8f647b8d0d7dfbe0
ViniUme <viniciushenrique.dev@gmail.com>
2025-09-03 18:38:24 -0300

    chore(routes): adjust api.login.auth route, remove duplicated prefix

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('api')->group(function () {
-    Route::post('login/auth', LoginAuthController::class)->name('api.login.auth');
-});
+Route::post('login/auth', LoginAuthController::class)->name('api.login.auth');
