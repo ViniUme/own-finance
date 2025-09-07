e5209fc676335a0a34a934600f17c92a6391a698
ViniUme <viniciushenrique.dev@gmail.com>
2025-09-07 14:09:01 -0300

    style(controllers): remove unused code in LoginAuthController

--- app/Http/Controllers/Auth/LoginAuthController.php
+++ app/Http/Controllers/Auth/LoginAuthController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginAuthRequest;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
 
 class LoginAuthController extends Controller
 {
