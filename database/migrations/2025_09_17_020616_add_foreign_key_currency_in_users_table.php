859a1bb7995f809b2d823ad1d50a40da59f89946
ViniUme <viniciushenrique.dev@gmail.com>
2025-09-16 23:24:08 -0300

    feat(database): create migration to add columns to users table

---
+++ database/migrations/2025_09_17_020616_add_foreign_key_currency_in_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Currency;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignIdFor(Currency::class);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['currency_id']);
+            $table->dropColumn('currency_id');
+            $table->dropSoftDeletes();
+        });
+    }
+};
