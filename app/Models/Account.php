3d2f3a37d944916e0a2d974c0f7a05b2def34e11
ViniUme <viniciushenrique.dev@gmail.com>
2025-09-17 21:37:56 -0300

    feat(model): create account model

---
+++ app/Models/Account.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    protected $table = 'accounts';
+    protected $fillable = [
+        'user_id',
+        'currency_id',
+        'name',
+        'type',
+        'color_hex',
+        'current_balance',
+    ];
+}
